5365bad707eb198a3a0c7825ac8eb8c41653ed7b
Egor Boev <egorcoders@gmail.com>
2023-03-17 12:42:44 +0300

    delete idea, scratch

--- scratch.py
+++ scratch.py
@@ -1,7 +1,13 @@
-import logging
 
+import logging
+logging.basicConfig(format='%(asctime)s | %(levelname)s: %(message)s', level=logging.NOTSET)
+logging.debug('Here you have some information for debugging.')
 
 def fn():
     return 2
 
+
 logging.info(fn())
+
+if __name__ == '__main__':
+    fn()
